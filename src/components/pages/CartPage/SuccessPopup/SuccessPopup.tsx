632cb2774c9f4dd45f1baba459e4126718662d4c
Whitelistedd <geforcexxy@gmail.com>
2022-10-16 19:40:55 +0300

    fixed cart success always showing

--- src/components/pages/CartPage/SuccessPopup/SuccessPopup.tsx
+++ src/components/pages/CartPage/SuccessPopup/SuccessPopup.tsx
@@ -20,7 +20,7 @@ export const SuccessPopup = ({ orderSuccess }: SuccessPopupProps) => {
 
   return (
     <AnimatePresence>
-      {true && (
+      {orderSuccess && (
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
